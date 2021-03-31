04745be638886d4d7935f790b936e56c7b052803
Stefan Langenmaier <stefan.langenmaier@gmail.com>
2021-04-01 00:18:19 +0200

    add dump tool

---
+++ dump-tool/scripts/dump.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+DATE=$(date +%Y%m%d)
+
+rm /transfer -rf || /bin/true
+mkdir /transfer
+
+for index in airrow airrow-trajectories airrow-points airrow-ratings
+do
+elasticdump \
+  --input=http://es:9200/$index \
+  --output=/transfer/$index.analyzer.json \
+  --type=analyzer
+elasticdump \
+  --input=http://es:9200/$index \
+  --output=/transfer/$index.mapping.json \
+  --type=mapping
+elasticdump \
+  --input=http://es:9200/airrow \
+  --output=/transfer/$index.data.json \
+  --type=data
+done
+
+tar cfz - /transfer/ | curl -k -T - -u "${SHARE_KEY}:" -H 'X-Requested-With: XMLHttpRequest' "https://cloud.geodiscovery.xn--rro-pla.de/public.php/webdav/airrow-${DATE}-esdump.tar.gz"
+tar cfz - /data/ | curl -k -T - -u "${SHARE_KEY}:" -H 'X-Requested-With: XMLHttpRequest' "https://cloud.geodiscovery.xn--rro-pla.de/public.php/webdav/airrow-${DATE}-filedump.tar.gz"
